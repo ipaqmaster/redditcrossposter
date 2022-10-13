89744b5fb1e2777fff4d5df2604e3f2ed9f3a045
Jared J <jared@jared-desktop>
2022-10-14 10:07:43 +1100

    Add min karma, move crossposter to class.

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class DB():
   def __init__(self):
     self.con = sqlite3.connect(scriptBasename + ".db")
     self.cur = self.con.cursor()
-    self.cur.execute("CREATE TABLE IF NOT EXISTS " + scriptBasename + "(sourceSub, submissionSourceId, destSub, submissionDestId, dateProcessed)")
+    self.cur.execute("CREATE TABLE IF NOT EXISTS " + scriptBasename + "(sourceSub, submissionSourceId, sourceScore, destSub, submissionDestId, dateProcessed)")
 
   def checkPost(self, submission):
     result = self.cur.execute(
@@ -45,18 +45,54 @@ class DB():
 
   def trackPost(self, submission):
     result = self.cur.execute(
-      "INSERT into %s(sourceSub, submissionSourceId, destSub) values('"'%s'"', '"'%s'"', '"'%s'"')" % (scriptBasename, submission.subreddit.display_name, submission.id, confJson['global']['subreddit_dest'])
+      "INSERT into %s(sourceSub, submissionSourceId, sourceScore, destSub) values('"'%s'"', '"'%s'"', '"'%s'"', '"'%s'"')" % (scriptBasename, submission.subreddit.display_name, submission.id, submission.score, confJson['global']['subreddit_dest'])
       )
-    self.con.commit()
-    print('Tracked post %s from %s' % (submission.id, submission.subreddit.display_name))
+    if self.con.commit():
+      print('\tTracked post.')
 
   def markXpostComplete(self, submission, result):
     now = time.time()
     result = self.cur.execute(
             "UPDATE %s SET submissionDestId = '"'%s'"', dateProcessed = '"'%s'"' where sourceSub = '"'%s'"' and submissionSourceId = '"'%s'"' and destSub = '"'%s'"'" % (scriptBasename, result.id, now, submission.subreddit.display_name, submission.id, confJson['global']['subreddit_dest'] )
       )
-    self.con.commit()
-    print('Tracked post %s from %s' % (submission.id, submission.subreddit.display_name))
+    if self.con.commit():
+      print('\tMarked post complete.')
+
+class CROSSPOSTER():
+  def __init__(self):
+    self.reddit = praw.Reddit(
+      client_id       = confJson['reddit']['client_id'],
+      client_secret   = confJson['reddit']['client_secret'],
+      username        = confJson['reddit']['username'],
+      password        = confJson['reddit']['password'],
+      user_agent      = confJson['global']['user_agent']
+    )
+
+    self.db = DB()
+
+  def crosspost(self):
+    for submission in self.reddit.subreddit(confJson['global']['subreddit_source']).hot(limit=15):
+     if not self.db.checkPost(submission):
+        print('Seen new submission: ' + submission.id)
+        if bool(confJson['global']['do_trackpost']):
+          self.db.trackPost(submission)
+        else:
+          print('\tTracking crossposts in db disabled in config')
+
+        if not submission.score > confJson['global']['source_post_min_score']:
+          print('\tSubmission score of ' + str(submission.score) + ' not at minimum: ' + str(confJson['global']['source_post_min_score']) )
+          continue
+
+        if bool(confJson['global']['do_crosspost']):
+          try:
+            result = submission.crosspost(confJson['global']['subreddit_dest'], title="[" + submission.title + "]", send_replies=True)
+            #breakpoint()
+            self.db.markXpostComplete(submission, result)
+          except Exception as e:
+            print('\tFailed to crosspost: ' + submission.id )
+            print(e)
+        else:
+          print('\tCrossposting disabled in config')
 
 
 
@@ -68,28 +104,9 @@ class DB():
 #
 
 def main():
-  reddit = praw.Reddit(
-    client_id       = confJson['reddit']['client_id'],
-    client_secret   = confJson['reddit']['client_secret'],
-    username        = confJson['reddit']['username'],
-    password        = confJson['reddit']['password'],
-    user_agent      = confJson['global']['user_agent']
-  )
 
-  db = DB()
-
-  for submission in reddit.subreddit(confJson['global']['subreddit_source']).hot(limit=15):
-    #breakpoint()
-    if not db.checkPost(submission):
-      print('See new submission: ' + submission.id)
-      db.trackPost(submission)
-      try:
-        result = submission.crosspost(confJson['global']['subreddit_dest'], title="[" + submission.title + "]", send_replies=True)
-        #breakpoint()
-        db.markXpostComplete(submission, result)
-      except Exception as e:
-        print('Failed to crosspost: ' + submission.id )
-        print(e)
+  crossposter = CROSSPOSTER()
+  crossposter.crosspost()
 
 if __name__ == '__main__':
   main()
